7c978a132517c0174db58361fe08b53310a8b26e
Sahil Meshram <sahilmeshram222@gmail.com>
2025-08-08 21:24:14 +0530

    added deployed backend route

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(cors({
-  origin: ["", "http://localhost:5173"],
+  origin: ["https://custom-form-builder-backend.onrender.com", "http://localhost:5173"],
   credentials: true
 }));
 app.use(express.json({ limit: "2mb" }))
