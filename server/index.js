1fb3daa8afdcfeda8b9ff513fe3f9e063fb28762
Sahil Meshram <sahilmeshram222@gmail.com>
2025-08-08 21:32:51 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(cors({
-  origin: ["https://custom-form-builder-backend.onrender.com", "http://localhost:5173"],
+  origin: ["https://custom-form-builder-ten.vercel.app", "http://localhost:5173"],
   credentials: true
 }));
 app.use(express.json({ limit: "2mb" }))
